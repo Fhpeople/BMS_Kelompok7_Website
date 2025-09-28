20cf8a3ef1a9179cd1bac115e0873ba7a630f0df
Fhpeople <fathiyahaya8c@gmail.com>
2025-09-28 21:57:51 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function()
+{
   const circles = document.querySelectorAll(".circle");
   const modal = document.getElementById("popupModal");
   const popupImg = document.getElementById("popupImg");
   const popupText = document.getElementById("popupText");
 
-  circles.forEach(circle => {
-    circle.addEventListener("click", () => {
+  circles.forEach(function(circle)
+  {
+    circle.addEventListener("click", function()
+    {
       const img = circle.getAttribute("data-img");
       const text = circle.getAttribute("data-text");
-
       popupImg.src = img;
       popupText.textContent = text;
-
       modal.style.display = "flex";
     });
   });
 
-  window.addEventListener("click", (e) => {
-    if (e.target === modal) {
+  window.addEventListener("click", function(event)
+  {
+    if (event.target === modal) 
+    {
       modal.style.display = "none";
     }
   });
